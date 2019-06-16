e7b49dfca13e4150144159a33ade6817e4ad425e
Adri <adrianvillalbasanchez@gmail.com>
2019-06-16 22:34:27 +0200

    little changes

--- client/src/components/events/EventItem.js
+++ client/src/components/events/EventItem.js
@@ -1,145 +1,145 @@
-import React, { useState, useEffect } from 'react';
-import ReactMapGL from 'react-map-gl';
-import axios from 'axios';
-import PropTypes from 'prop-types';
-import Moment from 'react-moment';
-import { connect } from 'react-redux';
-import { takeOffEvent, joinEvent, deleteEvent } from '../../actions/event';
+import React, { useState, useEffect } from "react";
+import ReactMapGL from "react-map-gl";
+import axios from "axios";
+import PropTypes from "prop-types";
+import Moment from "react-moment";
+import { connect } from "react-redux";
+import { takeOffEvent, joinEvent, deleteEvent } from "../../actions/event";
 
 const EventItem = ({
-    auth,
-    takeOffEvent,
-    joinEvent,
-    deleteEvent,
-    event: { _id, title, date, location, language, user, usersAttending }
+  auth,
+  takeOffEvent,
+  joinEvent,
+  deleteEvent,
+  event: { _id, title, date, location, language, user, usersAttending }
 }) => {
-    useEffect(() => {
-        getLatLang();
-        setViewport({
-            ...viewport,
-            width: window.innerWidth < 761 ? 300 : 500
-        });
-    }, []);
-
-    const [viewport, setViewport] = useState({
-        width: null,
-        height: 230,
-        latitude: 37.7577,
-        longitude: -122.4376,
-        zoom: 13
+  useEffect(() => {
+    getLatLang();
+    setViewport({
+      ...viewport,
+      width: window.innerWidth < 761 ? 300 : 500
     });
+  }, []);
 
-    const getLatLang = async () => {
-        try {
-            const res = await axios.get(`/events/${_id}/latlang`);
-            const latLon = [res.data[0].lat, res.data[0].lon];
-            setViewport({
-                ...viewport,
-                latitude: parseFloat(res.data[0].lat),
-                longitude: parseFloat(res.data[0].lon)
-            });
-            return latLon;
-        } catch (err) {
-            console.error(err.message);
-        }
-    };
+  const [viewport, setViewport] = useState({
+    width: null,
+    height: 230,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 13
+  });
 
-    const changeWidth = () => {
-        if (window.innerWidth <= 760) {
-            setViewport({
-                ...viewport,
-                width: 250
-            });
-        } else {
-            setViewport({
-                ...viewport,
-                width: 500
-            });
-        }
-    };
+  const getLatLang = async () => {
+    try {
+      const res = await axios.get(`/events/${_id}/latlang`);
+      const latLon = [res.data[0].lat, res.data[0].lon];
+      setViewport({
+        ...viewport,
+        latitude: parseFloat(res.data[0].lat),
+        longitude: parseFloat(res.data[0].lon)
+      });
+      return latLon;
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
-    const TOKEN =
-        'pk.eyJ1IjoiYWRyaXZzIiwiYSI6ImNqbnVmMmJqYjB2cnozcHM1Y2o4dHc4Z3QifQ.FibwG1DRrJ2LTDQfAcy99A';
+  const changeWidth = () => {
+    if (window.innerWidth <= 760) {
+      setViewport({
+        ...viewport,
+        width: 250
+      });
+    } else {
+      setViewport({
+        ...viewport,
+        width: 500
+      });
+    }
+  };
 
-    return (
-        <div>
-            <hr />
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-lg-4'>
-                        <p>
-                            <strong>Title:</strong> {title}
-                        </p>
-                        <p>
-                            <strong>Date: </strong>
-                            <Moment format='YYYY-MM-DD HH:mm'>{date}</Moment>
-                        </p>
-                        <p>
-                            <strong>Location:</strong> {location}
-                        </p>
-                        <p>
-                            <strong>Language:</strong> {language}
-                        </p>
-                        <p>
-                            <strong>Users attending:</strong>{' '}
-                            {usersAttending.length}
-                            {/* {usersAttending.map(u => {
+  const TOKEN =
+    "pk.eyJ1IjoiYWRyaXZzIiwiYSI6ImNqbnVmMmJqYjB2cnozcHM1Y2o4dHc4Z3QifQ.FibwG1DRrJ2LTDQfAcy99A";
+
+  return (
+    <div>
+      <hr />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4">
+            <p>
+              <strong>Title:</strong> {title}
+            </p>
+            <p>
+              <strong>Date: </strong>
+              <Moment format="YYYY-MM-DD HH:mm">{date}</Moment>
+            </p>
+            <p>
+              <strong>Location:</strong> {location}
+            </p>
+            <p>
+              <strong>Language:</strong> {language}
+            </p>
+            <p>
+              <strong>Users attending:</strong> {usersAttending.length}
+              {/* {usersAttending.map(u => {
                         return <span>{u.user}</span>;
                     })} */}
-                        </p>
-                    </div>
-                    <div className='col-lg-8 map-div'>
-                        <ReactMapGL
-                            width={viewport.width}
-                            height={viewport.height}
-                            latitude={viewport.latitude}
-                            longitude={viewport.longitude}
-                            zoom={viewport.zoom}
-                            mapboxApiAccessToken={TOKEN}
-                        />
-                    </div>
-                </div>
-            </div>
-
-            <div>
-                <div className='attending set-buttons'>
-                    <button
-                        onClick={e => joinEvent(_id)}
-                        className='btn btn-sm btn-clover btn-join'
-                    >
-                        <span>Join</span>
-                    </button>
-                    <button
-                        onClick={e => takeOffEvent(_id)}
-                        className='btn btn-sm btn-clover btn-take-off'
-                    >
-                        <span>Take off</span>
-                    </button>
-                </div>
-                {!auth.loading && user === auth.user._id && (
-                    <button
-                        onClick={e => deleteEvent(_id)}
-                        className='btn btn-sm btn-danger'
-                    >
-                        Delete
-                    </button>
-                )}
-            </div>
+            </p>
+          </div>
+          <div className="col-lg-8 map-div">
+            <ReactMapGL
+              width={viewport.width}
+              height={viewport.height}
+              latitude={viewport.latitude}
+              longitude={viewport.longitude}
+              mapStyle="mapbox://styles/mapbox/streets-v9"
+              zoom={viewport.zoom}
+              mapboxApiAccessToken={TOKEN}
+            />
+          </div>
         </div>
-    );
+      </div>
+
+      <div>
+        <div className="attending set-buttons">
+          <button
+            onClick={e => joinEvent(_id)}
+            className="btn btn-sm btn-clover btn-join"
+          >
+            <span>Join</span>
+          </button>
+          <button
+            onClick={e => takeOffEvent(_id)}
+            className="btn btn-sm btn-clover btn-take-off"
+          >
+            <span>Take off</span>
+          </button>
+        </div>
+        {!auth.loading && user === auth.user._id && (
+          <button
+            onClick={e => deleteEvent(_id)}
+            className="btn btn-sm btn-danger"
+          >
+            Delete
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 EventItem.propTypes = {
-    event: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired,
-    deleteEvent: PropTypes.func.isRequired
+  event: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  deleteEvent: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
-    auth: state.auth
+  auth: state.auth
 });
 
 export default connect(
-    mapStateToProps,
-    { joinEvent, takeOffEvent, deleteEvent }
+  mapStateToProps,
+  { joinEvent, takeOffEvent, deleteEvent }
 )(EventItem);
--- routes/api/event.js
+++ routes/api/event.js
@@ -1,190 +1,189 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
-const auth = require('../../middleware/auth');
-const Event = require('../../models/Event');
-const Profile = require('../../models/Profile');
-const User = require('../../models/User');
-const axios = require('axios');
+const { check, validationResult } = require("express-validator/check");
+const auth = require("../../middleware/auth");
+const Event = require("../../models/Event");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const axios = require("axios");
 
 // @route   POST api/events
 // @desc    Create a event
 // @access  Private
 router.post(
-    '/',
-    [
-        auth
-        // [
-        //     check('location', 'Location is required')
-        //         .not()
-        //         .isEmpty(),
-        //     check('language', 'Language is required')
-        //         .not()
-        //         .isEmpty(),
-        //     check('title', 'Title is required')
-        //         .not()
-        //         .isEmpty(),
-        //     check('date', 'Date is required')
-        //         .not()
-        //         .isEmpty()
-        // ]
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array });
-        }
-
-        try {
-            const { location, language, title, date, user } = req.body;
-
-            //const user = User.findById(req.user.id).select('-password');
-            const newEvent = new Event({
-                location,
-                language,
-                title,
-                date,
-                user: req.user.id
-            });
-
-            const event = await newEvent.save();
-
-            res.json(event);
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server Error');
-        }
+  "/",
+  [
+    auth
+    // [
+    //     check('location', 'Location is required')
+    //         .not()
+    //         .isEmpty(),
+    //     check('language', 'Language is required')
+    //         .not()
+    //         .isEmpty(),
+    //     check('title', 'Title is required')
+    //         .not()
+    //         .isEmpty(),
+    //     check('date', 'Date is required')
+    //         .not()
+    //         .isEmpty()
+    // ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array });
     }
+
+    try {
+      const { location, language, title, date, user } = req.body;
+
+      //const user = User.findById(req.user.id).select('-password');
+      const newEvent = new Event({
+        location,
+        language,
+        title,
+        date,
+        user: req.user.id
+      });
+
+      const event = await newEvent.save();
+
+      res.json(event);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
 );
 
 // @route   GET api/events
 // @desc    Get all events
 // @access  Private
-router.get('/', auth, async (req, res) => {
-    try {
-        const events = await Event.find().sort({ date: -1 });
-        res.json(events);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
+router.get("/", auth, async (req, res) => {
+  try {
+    const events = await Event.find().sort({ date: -1 });
+    res.json(events);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route   GET api/events/:id
 // @desc    Get one event by ID
 // @access  Private
-router.get('/:id', auth, async (req, res) => {
-    try {
-        const event = await Event.findById(req.params.id);
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
 
-        if (!event) {
-            return res.status(404).json({ msg: 'Event not found' });
-        }
-
-        res.json(event);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
+    if (!event) {
+      return res.status(404).json({ msg: "Event not found" });
     }
+
+    res.json(event);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route   DELETE api/events
 // @desc    Delete an event
 // @access  Private
-router.delete('/:id', auth, async (req, res) => {
-    try {
-        const event = await Event.findById(req.params.id);
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
 
-        // Check user
-        if (event.user.toString() !== req.user.id) {
-            return res.status(401).json({ msg: 'You are not authorized' });
-        }
-
-        await event.remove();
-
-        res.json({ msg: 'Event removed' });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
+    // Check user
+    if (event.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "You are not authorized" });
     }
+
+    await event.remove();
+
+    res.json({ msg: "Event removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route   PUT api/events/join/:id
 // @desc    Join an event
 // @access  Private
-router.put('/join/:id', auth, async (req, res) => {
-    try {
-        const event = await Event.findById(req.params.id);
+router.put("/join/:id", auth, async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
 
-        // Check if user has already joined
-        if (
-            event.usersAttending.filter(u => u.user.toString() === req.user.id)
-                .length > 0
-        ) {
-            return res.status(400).json({ msg: 'You have already joined' });
-        }
-
-        event.usersAttending.unshift({ user: req.user.id });
-
-        await event.save();
-        res.json(event.usersAttending);
-    } catch (err) {
-        console.error(err.message);
-        return res.status(500).send('Server error');
+    // Check if user has already joined
+    if (
+      event.usersAttending.filter(u => u.user.toString() === req.user.id)
+        .length > 0
+    ) {
+      return res.status(400).json({ msg: "You have already joined" });
     }
+
+    event.usersAttending.unshift({ user: req.user.id });
+
+    await event.save();
+    res.json(event.usersAttending);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server error");
+  }
 });
 
 // @route   PUT api/events/takeoff/:id
 // @desc    Take off from an event
 // @access  Private
-router.put('/takeoff/:id', auth, async (req, res) => {
-    try {
-        const event = await Event.findById(req.params.id);
+router.put("/takeoff/:id", auth, async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
 
-        // Check if user has already joined
-        if (
-            event.usersAttending.filter(u => u.user.toString() === req.user.id)
-                .length === 0
-        ) {
-            return res.status(400).json({ msg: 'You have not yet joined' });
-        }
-
-        // Get remove index
-        const removeIndex = event.usersAttending.map(u =>
-            u.user.toString().indexOf(req.user.id)
-        );
-
-        event.usersAttending.splice(removeIndex, 1);
-
-        await event.save();
-
-        res.json(event.usersAttending);
-    } catch (err) {
-        console.error(err.message);
-        return res.status(500).send('Server error');
+    // Check if user has already joined
+    if (
+      event.usersAttending.filter(u => u.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "You have not yet joined" });
     }
+
+    // Get remove index
+    const removeIndex = event.usersAttending.map(u =>
+      u.user.toString().indexOf(req.user.id)
+    );
+
+    event.usersAttending.splice(removeIndex, 1);
+
+    await event.save();
+
+    res.json(event.usersAttending);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server error");
+  }
 });
 
 // @route   GET api/events/latlang
 // @desc    Get lat and land from location
 // @access  Private
-router.get('/:id/latlang', async (req, res) => {
-    try {
-        const event = await Event.findById(req.params.id);
-        const location = event.location;
-        const locURL = location
-            .split(' ')
-            .join('%20')
-            .replace(',', '');
-        const latLang = await axios.get(
-            `https://eu1.locationiq.com/v1/search.php?key=80ea07bc0a0758&q=${locURL}=&format=json`
-        );
-        console.log(latLang.data);
-        res.json(latLang.data);
-    } catch (err) {
-        console.error(err.message);
-        return res.status(500).send('Server error');
-    }
+router.get("/:id/latlang", async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
+    const location = event.location;
+    const locURL = location
+      .split(" ")
+      .join("%20")
+      .replace(",", "");
+    const latLang = await axios.get(
+      `https://eu1.locationiq.com/v1/search.php?key=80ea07bc0a0758&q=${locURL}=&format=json`
+    );
+    res.json(latLang.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
